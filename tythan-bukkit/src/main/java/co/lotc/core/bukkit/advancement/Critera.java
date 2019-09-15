3e0f05bcfdb6a17c19a456f76d5d91ca44b680de
SeventhCircle <pokemonlove1389@gmail.com>
2019-09-15 10:39:34 -0400

    More Builders

--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/advancement/Critera.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/advancement/Critera.java
@@ -1,9 +1,15 @@
 package co.lotc.core.bukkit.advancement;
 
-import lombok.Builder;
-import org.bukkit.Material;
 
-@Builder
+import co.lotc.core.bukkit.advancement.enums.Triggers;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+
 public class Critera {
+	private String name;
+	private Triggers trigger;
+
+	ItemStack item =
 
 }
--- tythan-bukkit/src/main/java/co/lotc/core/bukkit/advancement/Icon.java
+++ tythan-bukkit/src/main/java/co/lotc/core/bukkit/advancement/Icon.java
@@ -1,9 +1,39 @@
 package co.lotc.core.bukkit.advancement;
+import org.bukkit.Material;
 
-import lombok.Builder;
-
-@Builder
 public class Icon {
-	private String itemid;
-	private String nbt;
+	private final String material;
+	private final String nbt;
+
+	private Icon(IconBuilder iconBuilder){
+		material = iconBuilder.material.getKey().toString();
+		nbt = iconBuilder.nbt;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public String getNbt() {
+		return nbt;
+	}
+
+	public static class IconBuilder{
+		private Material material;
+		private String nbt;
+
+		public IconBuilder setMaterial(Material material) {
+			this.material = material;
+			return this;
+		}
+
+		public IconBuilder setNbt(String nbt) {
+			this.nbt = nbt;
+			return this;
+		}
+
+		public Icon build(){
+			return new Icon(this);
+		}
+	}
 }
